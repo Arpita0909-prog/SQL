9dbb0555cbb7956e5a7f6f2e3b63173799dd91a3
Arpita0909-prog <manudwivedi044@gmail.com>
2025-12-19 06:26:09 -0800

    Adding multiple tables in sql

--- index.js
+++ index.js
@@ -12,21 +12,28 @@ connection.connect((err) => {
   if (err) {
     console.error('Error connecting to the database:', err);
     return;
-  }
+ }
     console.log('Connected to the MySQL database.');
-    const creationQuery = `create table students (
-    id INT PRIMARY KEY,
-    name VARCHAR(50),
-    age INT)`;
-    connection.query(creationQuery, (err, results) => {
-      if (err) {
-        console.error('Error creating table:', err);
-        return;
-      }
-        console.log('Table created successfully.');
-    });
 });
 
+    function createTable(query,tableName){
+        connection.query(query, (err, results) => {
+          if (err) {
+            console.error('Error creating table:', err);
+            return;
+          }
+            console.log('Table created successfully.');
+        });
+    }
+    const createUserTable = `create table users(id INT PRIMARY KEY AUTO_INCREMENT, name VARCHAR(50), email VARCHAR(50));`;
+    const createBusTable = `create table buses(id INT PRIMARY KEY AUTO_INCREMENT, busNumber VARCHAR(20), totalSeats INT, availableSeats INT);`;
+    const createBookingTable = `create table bookings(id INT PRIMARY KEY AUTO_INCREMENT, seatNumber INT);`;
+    const createPaymentTable = `create table payments(id INT PRIMARY KEY AUTO_INCREMENT, amountPaid DECIMAL(10,2), paymentStatus VARCHAR(20));`;
+
+    createTable(createUserTable,'users');
+    createTable(createBusTable,'buses');
+    createTable(createBookingTable,'bookings');
+    createTable(createPaymentTable,'payments');
 app.get('/', (req, res) => {
   res.send('Hello World!');
 });
